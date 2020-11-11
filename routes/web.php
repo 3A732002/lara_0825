31cfad6780becdf64d5ee86a4b5cecc56e2c8282
ma yu cen <sheehan010507@hotmail.com>
2020-11-11 11:08:17 +0800

    使用 destroy方法刪除第二筆資料

--- routes/web.php
+++ routes/web.php
@@ -49,9 +49,10 @@ Route::get('/', function () {
 //    $post->age='saved age';
 //    $post->save();
 
-    $post=Post::find(1);
-    $post->delete();
+//    $post=Post::find(1);
+//    $post->delete();
 
+    Post::destroy(2);
 
 });
 
