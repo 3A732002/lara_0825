eb2ccb875fd321a6c5f86591539992232df02a67
ma yu cen <sheehan010507@hotmail.com>
2020-11-11 11:02:53 +0800

    使用 update方法 (此修改資料的方式屬於mass assignment)

--- routes/web.php
+++ routes/web.php
@@ -33,8 +33,16 @@ Route::get('/', function () {
 //    $post=Post::find(1);
 //    dd($post);
 
-    $posts=Post::where('id','<',10)->orderBy('id','DESC')->get();
-    dd($posts);
+//    $posts=Post::where('id','<',10)->orderBy('id','DESC')->get();
+//    dd($posts);
+
+    $post=Post::find(1);
+    $post->update([
+        'title'=>'updated title',
+        'content'=>'updated content',
+        'age'=>'updated age'
+    ]);
+
 
 
 });
